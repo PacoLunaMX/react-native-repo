92cfc685d79c92f3d8797d1f62a6ed5a9477ec4f
PacoLunaMX <f.hinojosa9@hotmail.com>
2023-12-04 17:04:26 -0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
 
 export default function App() {
+
+  const  days = Array.from({length:24}, (_,i)=>i+1)
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <FlatList  
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({ item })=> <DayListItem day={item}/> }
+      />
+      
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -14,7 +28,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  
+  content:{
+    gap:10,
+    padding:10
+  },
+
+  column:{
+    gap:10
+  },
+
 });
